8b78afaabdc14f6b7615cd634b1916ebc31c7c9a
Ahmad Alsaleh <aalsaleh.at@emaar.ae>
2025-11-04 21:45:06 +0400

    remove a done todo

--- src/routers/companies.rs
+++ src/routers/companies.rs
@@ -5,10 +5,7 @@ pub fn get_router() -> Router {
     Router::new().route("/companies", get(get_all_companies))
 }
 
-async fn get_all_companies(jwt_token: JwtToken) -> Json<Vec<Company>> {
-    // TODO: ensure the user is authorized (ie logged in)
-    dbg!(jwt_token.user_id);
-
+async fn get_all_companies(_: JwtToken) -> Json<Vec<Company>> {
     // TODO: fetch all companies from the db using a state
     Json(vec![
         Company::new(String::from("Al Forsan")),
