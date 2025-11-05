c1faefa4595f1ec3e9fad574702170fca8db74d1
Ahmad Alsaleh <aalsaleh.at@emaar.ae>
2025-11-05 18:42:44 +0400

    rename models/logging.rs to models/request_log_info.rs

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,8 +1,8 @@
 mod company;
-mod logging;
+mod request_log_info;
 mod request_payloads;
 
 #[allow(unused)] // TODO: remove me
 pub use company::{Company, CompanyBuilder};
-pub use logging::RequestLogInfo;
+pub use request_log_info::RequestLogInfo;
 pub use request_payloads::LoginPayload;
