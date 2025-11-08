a5962067c98c5013168372f247008ebeb9f4d14d
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2025-11-08 10:25:07 +0400

    replace unwraps in configs.rs with expect

--- src/configs.rs
+++ src/configs.rs
@@ -15,15 +15,19 @@ pub struct Config {
 impl Config {
     fn load() -> Self {
         Self {
-            db_uri: std::env::var("DB_URI").unwrap(),
+            db_uri: std::env::var("DB_URI").expect("failed to load DB_URI"),
             server_address: std::env::var("SERVER_ADDRESS")
                 .unwrap_or_else(|_| String::from("127.0.0.1:1936")),
             jwt_exp_duration: Duration::from_secs(
                 std::env::var("JWT_EXP_DURATION_SECONDS")
-                    .map(|x| x.parse().unwrap())
+                    .map(|value| {
+                        value
+                            .parse()
+                            .expect("failed to parse JWT_EXP_DURATION_SECONDS as an int")
+                    })
                     .unwrap_or(60 * 15),
             ),
-            jwt_secret: std::env::var("JWT_SECRET").unwrap(),
+            jwt_secret: std::env::var("JWT_SECRET").expect("failed to load JWT_SECRET"),
         }
     }
 }
