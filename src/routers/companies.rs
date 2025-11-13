a655ce621a16901ac6db08744d96333860d9462b
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2025-11-13 18:30:48 +0400

    change the reponse of POST /companies to 201

--- src/routers/companies.rs
+++ src/routers/companies.rs
@@ -4,7 +4,10 @@ use crate::{
     middlewares::authenticate,
     models::{Company, CreateCompanyPayload, ModelManager},
 };
-use axum::{Json, Router, extract::State, middleware, routing::get};
+use axum::{
+    Json, Router, extract::State, http::StatusCode, middleware, response::IntoResponse,
+    routing::get,
+};
 use serde_json::{Value, json};
 
 pub fn get_router() -> Router<ModelManager> {
@@ -26,9 +29,14 @@ async fn get_all_companies(
 async fn create_company(
     State(model_manager): State<ModelManager>,
     Json(create_company_payload): Json<CreateCompanyPayload>,
-) -> Result<Json<Value>, ServerError> {
+) -> Result<impl IntoResponse, ServerError> {
     let id = CompanyController::create(&model_manager, &create_company_payload.name)
         .await
         .map_err(|err| ServerError::DataBase(err.to_string()))?;
-    Ok(Json(json!({"company_id": id})))
+
+    let response = json!({
+        "company_id": id
+    });
+
+    Ok((StatusCode::CREATED, Json(response)))
 }
