3b5000213b76b71cd9920e92858347a47dd62713
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2025-11-14 21:03:09 +0400

    split tests/ dir

--- tests/auth.rs
+++ tests/auth.rs
@@ -9,50 +9,6 @@ use uuid::Uuid;
 
 const DEV_BASE_URL: &str = "http://localhost:1948";
 
-#[tokio::test]
-async fn index() {
-    let client = httpc_test::new_client(DEV_BASE_URL).unwrap();
-    let response = client.do_get("/").await.unwrap();
-
-    let status = response.status();
-    let content_type = response.header("Content-Type").unwrap();
-    let response_body = response.text_body().unwrap();
-
-    assert_eq!(status, StatusCode::OK);
-    assert!(content_type.starts_with("text/html"));
-    assert_eq!(response_body, "<h1>Hello <i>World!</i></h1>");
-}
-
-#[tokio::test]
-async fn ping() {
-    let client = httpc_test::new_client(DEV_BASE_URL).unwrap();
-    let response = client.do_get("/ping").await.unwrap();
-
-    let status = response.status();
-    let content_type = response.header("Content-Type").unwrap();
-    let response_body = response.text_body().unwrap();
-
-    assert_eq!(status, StatusCode::OK);
-    assert!(content_type.starts_with("text/plain"));
-    assert_eq!(response_body, "pong!");
-}
-
-#[tokio::test]
-async fn fallback() {
-    let client = httpc_test::new_client(DEV_BASE_URL).unwrap();
-    let response = client.do_get("/does-not-exist").await.unwrap();
-
-    let status = response.status();
-    let content_type = response.header("Content-Type").unwrap();
-
-    assert!(content_type.starts_with("text/plain"));
-    assert_eq!(status, StatusCode::NOT_FOUND);
-    assert_eq!(
-        response.text_body().unwrap(),
-        "The specified endpoint `GET /does-not-exist` is not found."
-    );
-}
-
 // TODO: for the login endponit, test client cookies, etc. test cookies if we login then make another api call
 // an easy and (imo) good way is to make a /protected-ping endpoint (follow restful naming conventions) for tests only that will return `pong!`
 
