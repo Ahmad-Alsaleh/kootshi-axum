3f68ad6042e11398eba8111e6a2f744e1c5fb7ef
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2025-12-30 19:05:27 +0400

    use absolute paths and core instead of std inside `macro_rules!`

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -5,13 +5,13 @@ pub use client_error::ClientError;
 pub use server_error::ServerError;
 
 macro_rules! error_impl {
-    ($type: ty) => {
-        impl std::fmt::Display for $type {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                write!(f, "{self:?}")
+    ($type:ty) => {
+        impl ::core::fmt::Display for $type {
+            fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+                ::core::write!(f, "{self:?}")
             }
         }
-        impl std::error::Error for $type {}
+        impl ::core::error::Error for $type {}
     };
 }
 
--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,11 +1,17 @@
 macro_rules! login {
     ($client:expr, user = $user:ident) => {{
-        let login_body = serde_json::json!({"username": stringify!($user), "password": concat!(stringify!($user), "_password")});
+        let login_body = ::serde_json::json!({
+            "username": ::core::stringify!($user),
+            "password": ::core::concat!(::core::stringify!($user), "_password"),
+        });
         $client.do_post("/auth/login", login_body).await.unwrap();
     }};
 
     ($client:expr, username = $username:expr, password = $password:expr) => {{
-        let login_body = serde_json::json!({"username": $username, "password": $password});
+        let login_body = ::serde_json::json!({
+            "username": $username,
+            "password": $password,
+        });
         $client.do_post("/auth/login", login_body).await.unwrap();
     }};
 }
