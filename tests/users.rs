7d67945ffa4abb769bad2a4b5adcc4fd78cce400
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2026-01-04 16:20:09 +0400

    update tests/users.rs

--- tests/users.rs
+++ tests/users.rs
@@ -1,6 +1,8 @@
 mod utils;
 
+use rand::distr::{Alphanumeric, SampleString};
 use serde_json::json;
+use serial_test::serial;
 use utils::login;
 use uuid::Uuid;
 
@@ -114,195 +116,265 @@ async fn get_personal_info_err_login_needed() -> anyhow::Result<()> {
     Ok(())
 }
 
-// // PATCH /users/me 204
-// #[tokio::test]
-// // #[serial(user_table)] // TODO: insert a dummy user before updating to remove `serial`
-// async fn update_personal_info_ok_single_field() -> anyhow::Result<()> {
-//     let client = httpc_test::new_client(DEV_BASE_URL).unwrap();
-//
-//     // prepare
-//     login!(client, user = player_2);
-//
-//     // exec
-//     let new_last_name = Alphanumeric.sample_string(&mut rand::rng(), 16);
-//     let payload = json!({
-//         "last_name": new_last_name
-//     });
-//     let response = client.do_patch("/users/me", payload).await?;
-//
-//     // check status code
-//     assert_eq!(response.status(), 204, "response body:\n{response_body:#}");
-//
-//     // check response body
-//     assert!(response.json_body().is_err());
-//     assert!(response.text_body().is_err());
-//
-//     // check correct excution
-//     let response = client.do_get("/users/me").await?;
-//     let response_body = response.json_body().unwrap();
-//
-//     #[derive(Deserialize)]
-//     #[allow(unused)]
-//     struct Schema {
-//         id: Uuid,
-//         username: String,
-//         first_name: Option<String>,
-//         last_name: Option<String>,
-//     }
-//     let schema = Schema::deserialize(response_body)
-//         .context("response body does not match expected schema")?;
-//     assert_eq!(schema.username, "ahmad.alsaleh");
-//     assert_eq!(schema.first_name.as_deref(), Some("Ahmad"));
-//     assert_eq!(schema.last_name, Some(new_last_name));
-//
-//     // clean
-//     let payload = json!({
-//         "last_name": "Alsaleh",
-//     });
-//     let response = client.do_patch("/users/me", payload).await?;
-//     assert_eq!(response.status(), 204, "response body:\n{response_body:#}");
-//
-//     Ok(())
-// }
-//
-// // PATCH /users/me 204
-// #[tokio::test]
-// #[serial(user_table)]
-// async fn update_personal_info_ok_multiple_fields_and_a_null_field() -> anyhow::Result<()> {
-//     let client = httpc_test::new_client(DEV_BASE_URL).unwrap();
-//
-//     // prepare
-//     login!(client);
-//
-//     // exec
-//     let new_username = Uuid::new_v4().to_string(); // any random value
-//     let new_first_name = Uuid::new_v4().to_string(); // any random value
-//     let payload = json!({
-//         "username": new_username,
-//         "first_name": new_first_name,
-//         "last_name": null,
-//     });
-//     let response = client.do_patch("/users/me", payload).await?;
-//
-//     // check status code
-//     assert_eq!(response.status(), 204, "response body:\n{response_body:#}");
-//
-//     // check response body
-//     assert!(response.json_body().is_err());
-//     assert!(response.text_body().is_err());
-//
-//     // check correct excution
-//     let response = client.do_get("/users/me").await?;
-//     let response_body = response.json_body().unwrap();
-//
-//     #[derive(Deserialize)]
-//     #[allow(unused)]
-//     struct Schema {
-//         id: Uuid,
-//         username: String,
-//         first_name: Option<String>,
-//         last_name: Option<String>,
-//     }
-//     let schema = Schema::deserialize(response_body)
-//         .context("response body does not match expected schema")?;
-//     assert_eq!(schema.username, new_username);
-//     assert_eq!(schema.first_name, Some(new_first_name));
-//     assert_eq!(schema.last_name, None);
-//
-//     // clean
-//     let payload = json!({
-//         "username": "ahmad.alsaleh",
-//         "first_name": "Ahmad",
-//         "last_name": "Alsaleh",
-//     });
-//     let response = client.do_patch("/users/me", payload).await?;
-//     assert_eq!(response.status(), 204, "response body:\n{response_body:#}");
-//
-//     Ok(())
-// }
-//
-// // PATCH /users/me 204
-// #[tokio::test]
-// #[serial(user_table)]
-// async fn update_personal_info_ok_zero_fields() -> anyhow::Result<()> {
-//     let client = httpc_test::new_client(DEV_BASE_URL).unwrap();
-//
-//     // prepare
-//     login!(client);
-//
-//     // exec
-//     let payload = json!({});
-//     let response = client.do_patch("/users/me", payload).await?;
-//
-//     // check status code
-//     assert_eq!(response.status(), 204, "response body:\n{response_body:#}");
-//
-//     // check response body
-//     assert!(response.json_body().is_err());
-//     assert!(response.text_body().is_err());
-//
-//     // check correct excution
-//     let response = client.do_get("/users/me").await?;
-//     let response_body = response.json_body().unwrap();
-//
-//     #[derive(Deserialize)]
-//     #[allow(unused)]
-//     struct Schema {
-//         id: Uuid,
-//         username: String,
-//         first_name: Option<String>,
-//         last_name: Option<String>,
-//     }
-//     let schema = Schema::deserialize(response_body)
-//         .context("response body does not match expected schema")?;
-//     assert_eq!(schema.username, "ahmad.alsaleh");
-//     assert_eq!(schema.first_name.as_deref(), Some("Ahmad"));
-//     assert_eq!(schema.last_name.as_deref(), Some("Alsaleh"));
-//
-//     Ok(())
-// }
-//
-// // PATCH /users/me 204
-// #[tokio::test]
-// #[serial(user_table)]
-// async fn update_personal_info_ok_no_update() -> anyhow::Result<()> {
-//     let client = httpc_test::new_client(DEV_BASE_URL).unwrap();
-//
-//     // prepare
-//     login!(client);
-//
-//     // exec
-//     let payload = json!({
-//         "username": "ahmad.alsaleh",
-//         "first_name": "Ahmad",
-//         "last_name": "Alsaleh",
-//     });
-//     let response = client.do_patch("/users/me", payload).await?;
-//
-//     // check status code
-//     assert_eq!(response.status(), 204, "response body:\n{response_body:#}");
-//
-//     // check response body
-//     assert!(response.json_body().is_err());
-//     assert!(response.text_body().is_err());
-//
-//     // check correct excution
-//     let response = client.do_get("/users/me").await?;
-//     let response_body = response.json_body().unwrap();
-//
-//     #[derive(Deserialize)]
-//     #[allow(unused)]
-//     struct Schema {
-//         id: Uuid,
-//         username: String,
-//         first_name: Option<String>,
-//         last_name: Option<String>,
-//     }
-//     let schema = Schema::deserialize(response_body)
-//         .context("response body does not match expected schema")?;
-//     assert_eq!(schema.username, "ahmad.alsaleh");
-//     assert_eq!(schema.first_name.as_deref(), Some("Ahmad"));
-//     assert_eq!(schema.last_name.as_deref(), Some("Alsaleh"));
-//
-//     Ok(())
-// }
+// TODO: test update_personal_info_ok with player, business, and admin profiles
+
+// PATCH /users/me 204
+#[tokio::test]
+async fn update_personal_info_ok_player() -> anyhow::Result<()> {
+    let client = httpc_test::new_client(DEV_BASE_URL).unwrap();
+
+    // prepare
+    let username = Alphanumeric.sample_string(&mut rand::rng(), 16);
+    let password = Alphanumeric.sample_string(&mut rand::rng(), 16);
+    let first_name = Alphanumeric.sample_string(&mut rand::rng(), 16);
+    let last_name = Alphanumeric.sample_string(&mut rand::rng(), 16);
+    let preferred_sports = ["football", "basketball", "padel"];
+    let request_body = ::serde_json::json!({
+        "username": username,
+        "password": password,
+        "confirm_password": password,
+        "account_type": "player",
+        "profile": {
+            "first_name": first_name,
+            "last_name": last_name,
+            "preferred_sports": preferred_sports,
+        }
+    });
+    let response = client.do_post("/auth/signup", request_body).await.unwrap();
+    assert_eq!(response.status(), 201);
+    let response_body = response.json_body().unwrap();
+    let user_id = response_body.get("user_id").unwrap().as_str().unwrap();
+
+    login!(client, username = username, password = password);
+
+    // exec
+    let new_username = Alphanumeric.sample_string(&mut rand::rng(), 16);
+    let new_last_name = Alphanumeric.sample_string(&mut rand::rng(), 16);
+    let payload = json!({
+        "username": new_username,
+        "player_profile": {
+            "last_name": new_last_name,
+        }
+    });
+    let response = client.do_patch("/users/me", payload).await?;
+
+    // check status code
+    assert_eq!(response.status(), 204);
+
+    // check response body
+    assert!(response.json_body().is_err());
+    assert!(response.text_body().is_err());
+
+    // check correct excution
+    let response = client.do_get("/users/me").await?;
+    let response_body = response.json_body().unwrap();
+
+    let expected_response = json!({
+        "id": user_id,
+        "username": new_username,
+        "account_type": "player",
+        "profile": {
+            "first_name": first_name,
+            "last_name": new_last_name,
+            "preferred_sports": preferred_sports,
+        }
+    });
+    assert_eq!(response_body, expected_response);
+
+    Ok(())
+}
+
+// PATCH /users/me 204
+#[tokio::test]
+async fn update_personal_info_ok_business() -> anyhow::Result<()> {
+    let client = httpc_test::new_client(DEV_BASE_URL).unwrap();
+
+    // prepare
+    let username = Alphanumeric.sample_string(&mut rand::rng(), 16);
+    let password = Alphanumeric.sample_string(&mut rand::rng(), 16);
+    let display_name = Alphanumeric.sample_string(&mut rand::rng(), 16);
+    let request_body = ::serde_json::json!({
+        "username": username,
+        "password": password,
+        "confirm_password": password,
+        "account_type": "business",
+        "profile": {
+            "display_name": display_name,
+        }
+    });
+    let response = client.do_post("/auth/signup", request_body).await.unwrap();
+    assert_eq!(response.status(), 201);
+    let response_body = response.json_body().unwrap();
+    let user_id = response_body.get("user_id").unwrap().as_str().unwrap();
+
+    login!(client, username = username, password = password);
+
+    // exec
+    let new_password = Alphanumeric.sample_string(&mut rand::rng(), 16);
+    let new_display_name = Alphanumeric.sample_string(&mut rand::rng(), 16);
+    let payload = json!({
+        "password": new_password,
+        "business_profile": {
+            "display_name": new_display_name,
+        }
+    });
+    let response = client.do_patch("/users/me", payload).await?;
+
+    // check status code
+    assert_eq!(response.status(), 204);
+
+    // check response body
+    assert!(response.json_body().is_err());
+    assert!(response.text_body().is_err());
+
+    // check correct excution
+    let response = client.do_get("/users/me").await?;
+    let response_body = response.json_body().unwrap();
+
+    let expected_response = json!({
+        "id": user_id,
+        "username": username,
+        "account_type": "business",
+        "profile": {
+            "display_name": new_display_name,
+        }
+    });
+    assert_eq!(response_body, expected_response);
+
+    // test if password is updated by logging in
+    let response = login!(client, username = username, password = new_password);
+    assert_eq!(response.status(), 200);
+
+    Ok(())
+}
+
+#[tokio::test]
+#[serial(changes_admin_user)] // TODO: remove this by creating an endpoint that allows admins to create new admins
+async fn update_personal_info_ok_admin() -> anyhow::Result<()> {
+    let client = httpc_test::new_client(DEV_BASE_URL).unwrap();
+
+    // prepare
+    login!(client, user = admin);
+
+    // exec
+    let new_username = Alphanumeric.sample_string(&mut rand::rng(), 16);
+    let payload = json!({
+        "password": null,
+        "username": new_username,
+    });
+    let response = client.do_patch("/users/me", payload).await?;
+
+    // check status code
+    assert_eq!(response.status(), 204);
+
+    // check response body
+    assert!(response.json_body().is_err());
+    assert!(response.text_body().is_err());
+
+    // check correct excution
+    let response = client.do_get("/users/me").await?;
+    let response_body = response.json_body().unwrap();
+
+    let expected_response = json!({
+        "id": "00000000-0000-0000-0000-000000000005",
+        "username": new_username,
+        "account_type": "admin",
+    });
+    assert_eq!(response_body, expected_response);
+
+    // clean
+    let payload = json!({
+        "username": "admin",
+    });
+    let response = client.do_patch("/users/me", payload).await?;
+
+    // check status code
+    assert_eq!(response.status(), 204);
+
+    Ok(())
+}
+
+// PATCH /users/me 204
+#[tokio::test]
+async fn update_personal_info_ok_no_fields() -> anyhow::Result<()> {
+    let client = httpc_test::new_client(DEV_BASE_URL).unwrap();
+
+    // prepare
+    login!(client, user = business_2);
+
+    // exec
+    let payload = json!({});
+    let response = client.do_patch("/users/me", payload).await?;
+
+    // check status code
+    assert_eq!(response.status(), 204);
+
+    // check response body
+    assert!(response.json_body().is_err());
+    assert!(response.text_body().is_err());
+
+    // check correct excution
+    let response = client.do_get("/users/me").await?;
+    let response_body = response.json_body().unwrap();
+
+    let expected_response = json!({
+        "id": "00000000-0000-0000-0000-000000000004",
+        "username": "business_2",
+        "account_type": "business",
+        "profile": {
+            "display_name": "business_2_display",
+        }
+    });
+    assert_eq!(response_body, expected_response);
+
+    Ok(())
+}
+
+// PATCH /users/me 204
+#[tokio::test]
+async fn update_personal_info_ok_no_update() -> anyhow::Result<()> {
+    let client = httpc_test::new_client(DEV_BASE_URL).unwrap();
+
+    // prepare
+    login!(client, user = player_2);
+
+    // exec
+    let payload = json!({
+        "username": "player_2",
+        "password": "player_2_password",
+        "player_profile": {
+            "first_name": "player_2_first",
+            "last_name": "player_2_last",
+        }
+    });
+    let response = client.do_patch("/users/me", payload).await?;
+
+    // check status code
+    assert_eq!(response.status(), 204);
+
+    // check response body
+    assert!(response.json_body().is_err());
+    assert!(response.text_body().is_err());
+
+    // check correct excution
+    let response = client.do_get("/users/me").await?;
+    let response_body = response.json_body().unwrap();
+
+    let expected_response = json!({
+        "id": "00000000-0000-0000-0000-000000000002",
+        "username": "player_2",
+        "account_type": "player",
+        "profile": {
+            "first_name": "player_2_first",
+            "last_name": "player_2_last",
+            "preferred_sports": ["basketball", "padel"],
+        }
+    });
+    assert_eq!(response_body, expected_response);
+
+    // test if password is not updated by logging in using the same credentials
+    let response = login!(client, user = player_2);
+    assert_eq!(response.status(), 200);
+
+    Ok(())
+}
